4a60a844b6b2f1146f1cf87faae5a893808e9491
jaylynb26 <jab9633@nyu.edu>
2023-10-24 13:16:25 -0400

    Nav.js created, App.js updated

--- front-end/src/components/App.js
+++ front-end/src/components/App.js
@@ -3,6 +3,7 @@ import "../App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import About from "./About";
 import Title from "./Title";
+import Nav from "./Nav";
 import HomepageFeed from "./HomepageFeed";
 
 function App() {
@@ -15,6 +16,7 @@ function App() {
           <Route path="/about" element={<About />} />
         </Routes>
       </Router>
+      <Nav />
     </div>
   );
 }
---
+++ front-end/src/components/Nav.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import '../App.css';
+
+function Nav() {
+    return (
+      <div className="Nav">
+        <ul>
+            <li><a href="/">Home</a></li>
+            <li><a href="/search">Search</a></li>
+            <li><a href="/profile">Profile</a></li>
+            <li><a href="/about">About</a></li>
+        </ul>
+      </div>
+    );
+  }
+  
+  export default Nav;
