4d47e23cc5eeb094f68000894a9b57bdebad0db1
aleong2002 <aleong2002@gmail.com>
2023-10-24 21:50:39 -0400

    logged and signed

--- front-end/src/components/LogIn.js
+++ front-end/src/components/LogIn.js
@@ -1,12 +1,31 @@
 import React from 'react';
-import '../App.css';
+import { Link } from 'react-router-dom';
+import '../css/LogIn.css';
 
-function logIn() {
-  return (
-    <div className="logIn">
-      
+function LogIn() {
+  return(
+    <div className='LogIn'>
+      <h2> Log In</h2>  
+      <form action='/log-in' method='post' enctype='multipart/form-data'>
+        <div class="input-group">
+          <label for="username">Username: </label><br/>
+          <input type="text" id="username" name="username" placeholder="Enter username" required/>
+        </div>
+        <br/>
+        <div class="input-group">
+          <label for="password">Password: </label><br/>
+          <input type="text" id="password" name="password" placeholder="Enter password" required/>
+        </div>
+        <br/>
+        <div class="button">
+          <input type="submit" value="Enter"/>
+        </div>
+
+      </form>
+      <br/>
+      <Link to="/signup">Need an account? Click here to sign up</Link>
     </div>
-  );
+  )
 }
 
-export default logIn;
+export default LogIn;
--- front-end/src/components/about.js
+++ front-end/src/components/about.js
@@ -1,6 +1,5 @@
 import React from 'react';
-import '../App.css';
-
+import '../css/About.css';
 function About() {
   return (
     <div className="About">
