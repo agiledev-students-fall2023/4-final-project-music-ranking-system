7b0634f51a919d9c220cfc7b5a3c26866b71d393
aleong2002 <aleong2002@gmail.com>
2023-10-26 09:23:21 -0400

    Merge https://github.com/agiledev-students-fall2023/4-final-project-music-ranking-system into origin/task/38/aboutPage

--- front-end/src/components/Comment.js
+++ front-end/src/components/Comment.js
@@ -1,16 +1,31 @@
 import React, { useState } from 'react';
+import axios from 'axios'; 
 
-function Comment({ onSubmit }) {
+const Comment = ({ addCommentToList }) => {
+  const [user, setUser] = useState('');
   const [comment, setComment] = useState('');
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    onSubmit(comment);
-    setComment('');
-  };
+
+    axios
+      .post(`${process.env.REACT_APP_SERVER_HOSTNAME}/comments/save`, {
+        user: user,
+        comment: comment,
+      })
+      .then(response => {
+        addCommentToList(response.data)
+      })
+      .catch(err => {
+        console.log("Error posting data:", err)
+      })
+
+      setUser('');
+      setComment('');
+  }
 
   return (
-    <form onSubmit={handleSubmit} method="post" enctype="multipart/form-data">
+    <form className="CommentForm" onSubmit={handleSubmit} method="post" enctype="multipart/form-data">
         <div class="input-group">
           <label for="comment">Add Comment: </label><br/>
           <textarea id="song-comment" value={comment} name="song-comment" onChange={(e) => setComment(e.target.value)} placeholder="Enter a comment" rows="10"></textarea>
@@ -18,10 +33,10 @@ function Comment({ onSubmit }) {
         <br/>
     
         <div class="button">
-          <input type="submit" value="Enter"/>
+          <input type="submit" disabled={!comment} value="Enter"/>
         </div>
     </form>
   );
-}
+};
 
 export default Comment;
---
+++ front-end/src/components/FeedComponent.js
@@ -0,0 +1,39 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+export default function FeedComponent({ item }) {
+  const style = {
+    background: "lightgray",
+    padding: "20px",
+    border: "1px solid #ccc",
+    display: "flex",
+    alignitems: "center",
+    borderRadius: "10px",
+    margin: "10px",
+    width: "1000px",
+    height: "200px",
+  };
+  return (
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        margin: "10px",
+      }}
+    >
+      <div style={style}>
+        <img style={{ width: 200, height: 200 }} src={item.cover} alt="temp" />
+        <Link style={{ textDecoration: "none" }} to={`/post/${item.id}`}>
+          <div style={{ margin: 10 }}>
+            <h1>
+              {item.artist} -- {item.song}
+            </h1>
+            <h2>{item.rating}/10</h2>
+            <p>{item.review}</p>
+          </div>
+        </Link>
+      </div>
+    </div>
+  );
+};
