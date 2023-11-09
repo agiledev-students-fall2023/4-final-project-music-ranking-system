4ff7eeed808df846a9bc08a318f6397621b3e09c
Silver1793 <richardli1793@gmail.com>
2023-11-08 21:12:32 -0500

    added search route to backend

--- back-end/app.js
+++ back-end/app.js
@@ -2,12 +2,12 @@
 const express = require("express"); // CommonJS import style!
 const app = express(); // instantiate an Express object
 const axios = require("axios"); // middleware for making requests to APIs
-const morgan = require("morgan") // middleware for printing requests on terminal
+const morgan = require("morgan"); // middleware for printing requests on terminal
 const homePage = require("./routes/homePage");
 const song = require("./routes/song");
 const otherUserRoute = require("./routes/otherUser");
 const postRoute = require("./routes/postRoute");
-
+const searchRoute = require("./routes/search");
 
 // use express' builtin body-parser middleware to parse data included in a request
 app.use(express.json());
@@ -22,7 +22,7 @@ app.use(function (req, res, next) {
   next();
 });
 
-app.use(morgan("dev"))
+app.use(morgan("dev"));
 // we will put some server logic here later...
 app.use("/static", express.static("public"));
 
@@ -30,6 +30,7 @@ app.use("/", homePage);
 app.use("/song", song);
 app.use("/", otherUserRoute);
 app.use("/", postRoute);
+app.use("/search", searchRoute);
 
 // export the express app we created to make it available to other modules
 module.exports = app;
