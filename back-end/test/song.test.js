eb3041d945b239e7bea26ddcde6ca32ae31fc60c
al6862 <al6862@nyu.edu>
2023-11-13 12:51:10 -0500

    initial tests, something breaking in the get request test

---
+++ back-end/test/song.test.js
@@ -0,0 +1,39 @@
+const chai = require("chai");
+const chaiHttp = require("chai-http");
+const app = require("../app");
+const assert = require("assert");
+
+const expect = chai.expect;
+chai.use(chaiHttp);
+
+describe("POST request to /song/:songArtist/:songTitle/save", () => {
+  it("should respond with an HTTP 200 status code and a new post object in the response body, currently with any input", (done) => {
+    chai
+      .request(app)
+      .post("/song/songArtist/songTitle/save")
+      .send({user: "username", rating: 5, review: "testing review..."})
+      .end((err, res) => {
+        expect(res).to.have.status(200);
+        expect(res).to.be.json;
+        expect(res.body).to.be.a("object");
+        expect(res.body.user).to.be.a("string")
+        expect(res.body.rating).to.be.a("number")
+        expect(res.body.review).to.be.a("string")
+        done();
+      });
+  });
+});
+
+describe("GET request to /song/:songArtist/:songTitle", () => {
+    it("should respond with an HTTP 200 status code and a song object in the response body, currently with any input", (done) => {
+        chai
+          .request(app)
+          .get("/song/songArtist/songTitle")
+          .end((err, res) => {
+            expect(res).to.have.status(200);
+            expect(res).to.be.json;
+            expect(res.body).to.be.a("object");
+            done();
+          });
+      });
+});
