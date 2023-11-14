b41b96168f708298aa11b5620ab8e6ec21dda2ee
al6862 <al6862@nyu.edu>
2023-11-13 19:27:23 -0500

    worked on tests...further..

--- back-end/test/song.test.js
+++ back-end/test/song.test.js
@@ -24,16 +24,16 @@ describe("POST request to /song/:songArtist/:songTitle/save", () => {
   });
 });
 
-describe("GET request to /song/:songArtist/:songTitle", () => {
-    it("should respond with an HTTP 200 status code and a song object in the response body, currently with any input", (done) => {
-        chai
-          .request(app)
-          .get("/song/songArtist/songTitle")
-          .end((err, res) => {
-            expect(res).to.have.status(200);
-            expect(res).to.be.json;
-            expect(res.body).to.be.a("object");
-            done();
-          });
-      });
-});
+// describe("GET request to /song/:songArtist/:songTitle", () => {
+//     it("should respond with an HTTP 200 status code and a song object in the response body, currently with any input", (done) => {
+//         chai
+//           .request(app)
+//           .get("/song/songArtist/songTitle")
+//           .end((err, res) => {
+//             expect(res).to.have.status(200);
+//             // expect(res).to.be.json;
+//             // expect(res.body).to.be.a("object");
+//             done();
+//           });
+//       });
+// });
