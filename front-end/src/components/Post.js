db1916b60de955978bbf9c9c7a398a7ad51d4840
aleong2002 <aleong2002@gmail.com>
2023-11-25 19:42:45 -0500

    post links updated
    links updated so that current users will be directed to profile-review and when viewing other user reviews, you can be directed to their profiles

--- front-end/src/components/Post.js
+++ front-end/src/components/Post.js
@@ -3,12 +3,14 @@ import '../css/Post.css';
 import { useParams, Link } from "react-router-dom";
 import axios from 'axios';
 import CommentDisplay from './CommentDisplay';
+import { useAuthContext } from "./AuthProvider.js";
 
  
 
 function Post() {
   //const { postId } = useParams(); // "1"
   const {username} = useParams();
+  const currentuser = useAuthContext().user;
   const [posts, setPosts] = useState([]);
   const {songArtist, songTitle} = useParams()
   const [song, setSong] = useState([])
@@ -22,6 +24,8 @@ function Post() {
         setSong(postResponse.song)
         setPosts(postResponse.posts);
         console.log(postResponse.posts);
+        console.log("Current", currentuser);
+        console.log("Username:", username);
       })
       .catch(err => {
         console.log("Error fetching data:", err)
@@ -32,20 +36,37 @@ function Post() {
     if (!song) {
       return <div>Loading...</div>
     }
-    return (
-      <div className="Post">
-        <h3><Link to={`/other-user/${username}`}>{username}</Link>'s Review</h3>
-        <h3>{song.artist} - {song.title}</h3>
-        <img src={song.coverSrc} alt="album cover" />
-        <br/>
-        <p><Link to='/other-user/ss'>ss</Link>  -- comment</p>
-        <p><Link to='/other-user/test1'>test1</Link> -- comment</p>
-        <br />
-        <CommentDisplay />
-  
-      </div>
-    );
-  }
+    if (currentuser == username) {
+      return (
+        <div className="Post">
+          <h3><Link to={`/profile-review`}>{username}</Link>'s Review</h3>
+          <h3>{song.artist} - {song.title}</h3>
+          <img src={song.coverSrc} alt="album cover" />
+          <br/>
+          <p><Link to='/other-user/ss'>ss</Link>  -- comment</p>
+          <p><Link to='/other-user/test1'>test1</Link> -- comment</p>
+          <br />
+          <CommentDisplay />
+    
+        </div>
+      );
+    }
+    else {
+      return (
+        <div className="Post">
+          <h3><Link to={`/other-user/${username}`}>{username}</Link>'s Review</h3>
+          <h3>{song.artist} - {song.title}</h3>
+          <img src={song.coverSrc} alt="album cover" />
+          <br/>
+          <p><Link to='/other-user/ss'>ss</Link>  -- comment</p>
+          <p><Link to='/other-user/test1'>test1</Link> -- comment</p>
+          <br />
+          <CommentDisplay />
+    
+        </div>
+      );
+    }
+  } 
   else {
     const filteredPosts = posts.map(post => {
       if (post.username === username) {
@@ -64,23 +85,36 @@ function Post() {
     if (!song) {
       return <div>Loading...</div>
     }
-    return (
-      <div className="Post">
-        <h3><Link to={`/other-user/${username}`}>{username}</Link>'s Review</h3>
-        <h3>{song.artist} - {song.title}</h3>
-        <img src={song.coverSrc} alt="album cover" />
-        <br/>
-  
-        {filteredPosts}
-        
-        <br/>
-        <p><Link to='/other-user/ss'>ss</Link>  -- comment</p>
-        <p><Link to='/other-user/test1'>test1</Link> -- comment</p>
-        <br />
-        <CommentDisplay />
-  
-      </div>
-    );
+    if (currentuser == username) {
+      return (
+        <div className="Post">
+          <h3><Link to={`/profile-review`}>{username}</Link>'s Review</h3>
+          <h3>{song.artist} - {song.title}</h3>
+          <img src={song.coverSrc} alt="album cover" />
+          <br/>
+          <p><Link to='/other-user/ss'>ss</Link>  -- comment</p>
+          <p><Link to='/other-user/test1'>test1</Link> -- comment</p>
+          <br />
+          <CommentDisplay />
+    
+        </div>
+      );
+    }
+    else {
+      return (
+        <div className="Post">
+          <h3><Link to={`/other-user/${username}`}>{username}</Link>'s Review</h3>
+          <h3>{song.artist} - {song.title}</h3>
+          <img src={song.coverSrc} alt="album cover" />
+          <br/>
+          <p><Link to='/other-user/ss'>ss</Link>  -- comment</p>
+          <p><Link to='/other-user/test1'>test1</Link> -- comment</p>
+          <br />
+          <CommentDisplay />
+    
+        </div>
+      );
+    }
   }
   
 }
--- front-end/src/components/SongPost.js
+++ front-end/src/components/SongPost.js
@@ -1,5 +1,4 @@
 import { Link } from "react-router-dom";
-import { useAuthContext } from "./AuthProvider.js";
 
 
 const SongPost = ({ post, songArtist, songTitle }) => {
