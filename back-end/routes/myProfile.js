788c1fc076b813e9a1171661675691a00f504012
gayatririthe <rithe.gayatri@gmail.com>
2023-11-26 21:49:36 -0800

    fixed bug v1

--- back-end/routes/myProfile.js
+++ back-end/routes/myProfile.js
@@ -47,20 +47,26 @@ const songModel = mongoose.model("Song", songSchema);
 const activityModel = mongoose.model("Activity", activitySchema);
 
 router.get("/songs", async (req, res) => {
-  try {
+  try 
+  {
     const songs = await songModel.find().limit(4);
     res.json(songs);
-  } catch (error) {
+  } 
+  catch (error) 
+  {
     console.error("Error fetching song data: ", error);
     res.status(500).json({ error: "Failed to fetch song data" });
   }
 });
 
 router.get("/activities", async (req, res) => {
-  try {
+  try 
+  {
     const activities = await activityModel.find().limit(3);
     res.json(activities);
-  } catch (error) {
+  } 
+  catch (error) 
+  {
     console.error("Error fetching activity data: ", error);
     res.status(500).json({ error: "Failed to fetch activity data" });
   }
@@ -68,34 +74,3 @@ router.get("/activities", async (req, res) => {
 
 module.exports = router;
 
-
-/*
-router.get("/songs", (req, res) => {
-  const response = axios
-   .get("https://api.mockaroo.com/api/4f9a5d40?count=4&key=deb8cfd0")
-    .then((apiResponse) => {
-      const resdata = apiResponse.data;
-      res.json(resdata);
-    })
-    .catch((error) => {
-      console.error("Error fetching song data: ", error);
-      res.status(500).json({ error: "Failed to fetch song data" });
-    });
-});
-
-router.get("/activities", (req, res) => {
-  const response = axios
-    .get("https://api.mockaroo.com/api/9360e250?count=3&key=deb8cfd0")
-    .then((apiResponse) => {
-      const resdata = apiResponse.data;
-      res.json(resdata);
-    })
-    .catch((error) => {
-      console.error("Error fetching activity data: ", error);
-      res.status(500).json({ error: "Failed to fetch activity data" });
-    });
-});
-
-module.exports = router;
-
-*/
