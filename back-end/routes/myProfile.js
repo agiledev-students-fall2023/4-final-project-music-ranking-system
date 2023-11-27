d46ab109f0a8b4ae34b457fc026ad9fdd10b9343
gayatririthe <rithe.gayatri@gmail.com>
2023-11-26 21:46:21 -0800

    took out unnecesssary code

--- back-end/routes/myProfile.js
+++ back-end/routes/myProfile.js
@@ -4,29 +4,51 @@ const router = express.Router();
 const mongoose = require("mongoose");
 require("dotenv").config();
 
-
-
-mongoose.connect("mongodb://localhost:27017/your_database_name", {
+mongoose.connect(process.env.MONGODB_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
 
 const songSchema = new mongoose.Schema({
-  title: String,
-  artist: String,
+  artistName: {
+    type: String,
+    required: true,
+  },
+  songName: {
+    type: String,
+    required: true,
+  },
+  albumCover: {
+    type: String,  
+    required: true,
+  }
 });
 
 const activitySchema = new mongoose.Schema({
-  name: String,
-  description: String,
+  artistName: {
+    type: String,
+    required: true,
+  },
+  songName: {
+    type: String,
+    required: true,
+  },
+  rating: {
+    type: Number,
+    required: true,
+  },
+  review: {
+    type: String,
+    required: true,
+  },
 });
 
-const Song = mongoose.model("Song", songSchema);
-const Activity = mongoose.model("Activity", activitySchema);
+const songModel = mongoose.model("Song", songSchema);
+const activityModel = mongoose.model("Activity", activitySchema);
 
 router.get("/songs", async (req, res) => {
   try {
-    const songs = await Song.find().limit(4);
+    const songs = await songModel.find().limit(4);
     res.json(songs);
   } catch (error) {
     console.error("Error fetching song data: ", error);
@@ -36,7 +58,7 @@ router.get("/songs", async (req, res) => {
 
 router.get("/activities", async (req, res) => {
   try {
-    const activities = await Activity.find().limit(3);
+    const activities = await activityModel.find().limit(3);
     res.json(activities);
   } catch (error) {
     console.error("Error fetching activity data: ", error);
