040e36d20ef814291ab9df7f7d1d49295e04443d
gayatririthe <rithe.gayatri@gmail.com>
2023-11-26 21:37:52 -0800

    forget env requriement, added it

--- back-end/routes/myProfile.js
+++ back-end/routes/myProfile.js
@@ -2,6 +2,8 @@ const express = require("express");
 const axios = require("axios");
 const router = express.Router();
 const mongoose = require("mongoose");
+require("dotenv").config();
+
 
 
 mongoose.connect("mongodb://localhost:27017/your_database_name", {
