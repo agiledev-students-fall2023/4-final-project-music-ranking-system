0a390a7c4475fe9d64f72f71cbdc13be591fdcd6
al6862 <al6862@nyu.edu>
2023-11-28 08:35:47 -0500

    dont need to connect to db, already connected in app.js

--- back-end/routes/myProfile.js
+++ back-end/routes/myProfile.js
@@ -1,50 +1,6 @@
 const express = require("express");
 const axios = require("axios");
 const router = express.Router();
-const mongoose = require("mongoose");
-require("dotenv").config();
-
-mongoose.connect(process.env.MONGODB_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
-
-const songSchema = new mongoose.Schema({
-  artistName: {
-    type: String,
-    required: true,
-  },
-  songName: {
-    type: String,
-    required: true,
-  },
-  albumCover: {
-    type: String,  
-    required: true,
-  }
-});
-
-const activitySchema = new mongoose.Schema({
-  artistName: {
-    type: String,
-    required: true,
-  },
-  songName: {
-    type: String,
-    required: true,
-  },
-  rating: {
-    type: Number,
-    required: true,
-  },
-  review: {
-    type: String,
-    required: true,
-  },
-});
-
-const songModel = mongoose.models.Song || mongoose.model("Song", songSchema);
-const activityModel = mongoose.models.Activity || mongoose.model("Activity", activitySchema);
 
 router.get("/songs", async (req, res) => {
   try 
