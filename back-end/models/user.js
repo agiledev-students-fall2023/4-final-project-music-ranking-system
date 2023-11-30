6fd56f7a8b7a5313aafa6c54b689fdc15300e25f
al6862 <al6862@nyu.edu>
2023-11-30 16:54:10 -0500

    added user schema

--- back-end/models/user.js
+++ back-end/models/user.js
@@ -1,35 +1,12 @@
-const mongoose = require("mongoose");
+// a mongoose model of a user
+const mongoose = require("mongoose")
+const Schema = mongoose.Schema
+const bcrypt = require("bcryptjs")
+const jwt = require("jsonwebtoken")
+// const jwtStrategy = require("../config/jwt-config.js") // import setup options for using JWT in passport
 
-const songSchema = new mongoose.Schema({
-  songName: {
-    type: String,
-    required: true,
-  },
-  artistName: {
-    type: String,
-    required: true,
-  },
-  albumCover: {
-    type: String,
-  },
-});
-
-const activitySchema = new mongoose.Schema({
-  review: {
-    type: String,
-  },
-  rating: {
-    type: Number,
-  },
-  songName: {
-    type: String,
-  },
-  artistName: {
-    type: String,
-  },
-});
-
-const userSchema = new mongoose.Schema({
+// this is our mongoose model for a user
+const UserSchema = new Schema({
   username: {
     type: String,
     unique: true,
@@ -39,12 +16,55 @@ const userSchema = new mongoose.Schema({
     type: String,
     required: true,
   },
-  topSongs: [songSchema],
-  activity: [activitySchema],
-});
+})
 
-userSchema.methods.comparePassword = function (password) {
-  return this.password === password;
-};
+// hash the password before the user is saved
+// mongoose provides hooks that allow us to run code before or after specific events
+UserSchema.pre("save", function (next) {
+  const user = this
+  // if the password has not changed, no need to hash it
+  if (!user.isModified("password")) return next()
+  // otherwise, the password is being modified, so hash it
+  bcrypt.hash(user.password, 10, (err, hash) => {
+    if (err) return next(err)
+    user.password = hash // update the password to the hashed version
+    next()
+  })
+})
 
-module.exports = mongoose.model("User", userSchema);
+// mongoose allows us to attach methods to a model...
+
+// compare a given password with the database hash
+UserSchema.methods.validPassword = function (password) {
+  return bcrypt.compareSync(password, this.password)
+}
+
+// return a JWT token for the user
+UserSchema.methods.generateJWT = function () {
+  const today = new Date()
+  const exp = new Date(today)
+  exp.setDate(today.getDate() + process.env.JWT_EXP_DAYS) // assuming an environment variable with num days in it
+
+  return jwt.sign(
+    {
+      id: this._id,
+      username: this.username,
+      exp: parseInt(exp.getTime() / 1000),
+    },
+    process.env.JWT_SECRET
+  )
+}
+
+// return the user information without sensitive data
+UserSchema.methods.toAuthJSON = function () {
+  return {
+    username: this.username,
+    token: this.generateJWT(),
+  }
+}
+
+// create a model from this schema
+const User = mongoose.model("User", UserSchema)
+
+// export the model
+module.exports = User
