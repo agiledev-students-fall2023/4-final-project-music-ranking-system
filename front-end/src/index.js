e1651a80be607b061bd8de23aadb4972b4630043
al6862 <al6862@nyu.edu>
2023-12-02 13:36:40 -0500

    removed context wrapper

--- front-end/src/index.js
+++ front-end/src/index.js
@@ -9,10 +9,8 @@ import AuthProvider from './components/AuthProvider.js';
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   // <React.StrictMode>
-  <AuthProvider>
     <App />
-  </AuthProvider>
-  /* </React.StrictMode> */
+  // </React.StrictMode>
 );
 
 <script src="https://unpkg.com/react-router-dom/umd/react-router-dom.min.js"></script>;
