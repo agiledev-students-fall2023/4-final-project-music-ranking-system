65c0dd1583f0cf2ed20dd10a9e61ea53910da590
al6862 <al6862@nyu.edu>
2023-12-02 13:43:40 -0500

    added logout

--- front-end/src/components/Settings.js
+++ front-end/src/components/Settings.js
@@ -10,25 +10,7 @@ function Settings() {
   console.log(authContext.auth)
 
   const logout = e => {
-    // e.preventDefault() // prevent normal browser submit behavior
-
-    // send data to server... getting server host name from .env environment variables file to make it easy to swap server hosts in one place
-    // maybe set property { withCredentials: true }?
-    axios
-      .get(`${process.env.REACT_APP_SERVER_HOSTNAME}/logout`)
-      .then(response => {
-        // prob want to redirect here once backend implemented?
-        // https://stackoverflow.com/questions/34735580/how-to-do-a-redirect-to-another-route-with-react-routers
-        console.log("Logged out successfully")
-      })
-      .catch(err => {
-        console.log("Error getting data:", err)
-      })
-    // for now, setting auth is false, removing local storage item auth and redirecting to / regardless of result of get request
-    authContext.setAuth(false)
-    authContext.setUser(null)
-    localStorage.removeItem("auth")
-    localStorage.removeItem("username")
+    localStorage.removeItem("token")
     navigate("/")
   }
   
