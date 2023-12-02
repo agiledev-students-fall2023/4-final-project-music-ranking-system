dbe4622e9a063bd23705551ae3cb401f35548f32
al6862 <al6862@nyu.edu>
2023-12-02 13:39:27 -0500

    actually adding it back for now

--- front-end/src/index.js
+++ front-end/src/index.js
@@ -9,8 +9,10 @@ import AuthProvider from './components/AuthProvider.js';
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   // <React.StrictMode>
+  <AuthProvider>
     <App />
-  // </React.StrictMode>
+  </AuthProvider>
+  /* </React.StrictMode> */
 );
 
 <script src="https://unpkg.com/react-router-dom/umd/react-router-dom.min.js"></script>;
