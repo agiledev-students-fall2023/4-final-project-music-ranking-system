6e586756a0ff66acfc8a5c55470fe52cec3ea1e2
Silver1793 <richardli1793@gmail.com>
2023-12-04 18:22:00 -0500

    css changes

--- front-end/src/components/FeedComponent.js
+++ front-end/src/components/FeedComponent.js
@@ -2,11 +2,10 @@ import React from "react";
 import { Link } from "react-router-dom";
 import "../css/FeedComponent.css";
 
-
-export default function FeedComponent({ item, rating, review }) {
+export default function FeedComponent({ username, item, rating, review }) {
   return (
     <div className="FeedComponent">
-        <div>
+      <div>
         <h1>
           <Link to={`/song/${item.artistName}/${item.songName}`}>
             {item.artistName} -- {item.songName}
@@ -14,6 +13,7 @@ export default function FeedComponent({ item, rating, review }) {
         </h1>
         <h1>{item.artistName}</h1>
         <img src={item.albumCover} alt="temp" />
+        {username && <h1>Posted By: {username}</h1>}
         {rating && <h2>{rating}/10</h2>}
         {review && <p>{review}</p>}
       </div>
