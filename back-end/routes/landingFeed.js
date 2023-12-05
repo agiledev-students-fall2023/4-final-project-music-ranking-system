1bf7f4d10473f3eef8591e4811f3eecd18a3227f
gayatririthe <rithe.gayatri@gmail.com>
2023-12-05 13:19:15 -0500

    made some edits

--- back-end/routes/landingFeed.js
+++ back-end/routes/landingFeed.js
@@ -4,35 +4,36 @@ const app = express(); // instantiate an Express object
 const axios = require("axios"); // middleware for making requests to APIs
 const router = require("express").Router();
 const mongoose = require("mongoose");
+const Song = require("../models/song.js")
 require("dotenv").config();
 
-mongoose.connect(process.env.MONGODB_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+// mongoose.connect(process.env.MONGODB_URI, {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+// });
 
-const landingFeedSchema = new mongoose.Schema({
-  artist: {
-    type: String,
-    required: true,
-  },
-  song: {
-    type: String,
-    required: true,
-  },
-  cover: {
-    type: String, // img url?
-    required: true,
-  },
-  rating: {
-    type: Number,
-    required: true,
-  },
-  review: {
-    type: String,
-    required: true,
-  },
-});
+// const landingFeedSchema = new mongoose.Schema({
+//   artist: {
+//     type: String,
+//     required: true,
+//   },
+//   song: {
+//     type: String,
+//     required: true,
+//   },
+//   cover: {
+//     type: String, // img url?
+//     required: true,
+//   },
+//   rating: {
+//     type: Number,
+//     required: true,
+//   },
+//   review: {
+//     type: String,
+//     required: true,
+//   },
+// });
 // const model = mongoose.model("Data", landingFeedSchema);
 
 // router.get("/retrieve", async (req, res) => {
@@ -57,26 +58,30 @@ const landingFeedSchema = new mongoose.Schema({
 // module.exports = router;
 
 
-const model = mongoose.model("Data", landingFeedSchema);
+//ADD CODE SO THAT IF THERE ARE NO REVIEWS IT SHOWS 3 RANDOM SONGS?
 
-router.get("/retrieve", async (req, res) => {
-  try {
-    const data = await model.find({}).exec();
-    res.json(data);
-  } catch (error) {
-    console.error("Error: ", error);
-    res.status(500).json({ error: "Internal Server Error" });
-  }
-});
+//const model = mongoose.model("Data", landingFeedSchema);
+
+// router.get("/retrieve", async (req, res) => {
+//   try {
+//     const data = await model.find({}).exec();
+//     res.json(data);
+//   } catch (error) {
+//     console.error("Error: ", error);
+//     res.status(500).json({ error: "Internal Server Error" });
+//   }
+// });
 
 router.get("/topSongs", async (req, res) => {
   try {
-    const topSongs = await model
-      .find({})
-      .sort({ numReviews: -1 })
-      .limit(3)
-      .exec();
+    const topSongs = await Song
+      .findOne({})
+      // .sort({ numReviews: -1 })
+      // .limit(3)
+      // .exec();
 
+
+      console.log(topSongs)
     res.json(topSongs);
   } catch (error) {
     console.error("Error: ", error);
