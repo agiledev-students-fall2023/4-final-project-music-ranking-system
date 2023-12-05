9d16c7e90b28f6984385cc4d7d64b327c08d52fe
gayatririthe <rithe.gayatri@gmail.com>
2023-12-05 13:01:40 -0500

    removed mockaroo stuff

--- back-end/routes/landingFeed.js
+++ back-end/routes/landingFeed.js
@@ -33,21 +33,51 @@ const landingFeedSchema = new mongoose.Schema({
     required: true,
   },
 });
+// const model = mongoose.model("Data", landingFeedSchema);
+
+// router.get("/retrieve", async (req, res) => {
+//   try {
+//     const data = await model.find({}).exec();
+//     if (data.length === 0) {
+//       const response = await axios.get(
+//         "https://api.mockaroo.com/api/d8caa150?count=3&key=9b1fc5d0"
+//       );
+//       const responseData = response.data;
+//       await model.insertMany(responseData);
+//       res.json(resdata);
+//     } else {
+//       res.json(data);
+//     }
+//   } catch (error) {
+//     console.error("Error: ", error);
+//     res.status(500).json({ error: "Internal Server Error" });
+//   }
+// });
+
+// module.exports = router;
+
+
 const model = mongoose.model("Data", landingFeedSchema);
 
 router.get("/retrieve", async (req, res) => {
   try {
     const data = await model.find({}).exec();
-    if (data.length === 0) {
-      const response = await axios.get(
-        "https://api.mockaroo.com/api/d8caa150?count=3&key=9b1fc5d0"
-      );
-      const responseData = response.data;
-      await model.insertMany(responseData);
-      res.json(resdata);
-    } else {
-      res.json(data);
-    }
+    res.json(data);
+  } catch (error) {
+    console.error("Error: ", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
+
+router.get("/topSongs", async (req, res) => {
+  try {
+    const topSongs = await model
+      .find({})
+      .sort({ numReviews: -1 })
+      .limit(3)
+      .exec();
+
+    res.json(topSongs);
   } catch (error) {
     console.error("Error: ", error);
     res.status(500).json({ error: "Internal Server Error" });
@@ -55,3 +85,4 @@ router.get("/retrieve", async (req, res) => {
 });
 
 module.exports = router;
+
