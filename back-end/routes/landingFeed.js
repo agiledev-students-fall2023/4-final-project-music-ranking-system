a8cd3c98f692b212401a5ae7e73641ebac5a8050
gayatririthe <rithe.gayatri@gmail.com>
2023-12-06 08:46:02 -0500

    removed unnecessary code

--- back-end/routes/landingFeed.js
+++ back-end/routes/landingFeed.js
@@ -7,70 +7,6 @@ const mongoose = require("mongoose");
 const Song = require("../models/song.js");
 require("dotenv").config();
 
-// mongoose.connect(process.env.MONGODB_URI, {
-//   useNewUrlParser: true,
-//   useUnifiedTopology: true,
-// });
-
-// const landingFeedSchema = new mongoose.Schema({
-//   artist: {
-//     type: String,
-//     required: true,
-//   },
-//   song: {
-//     type: String,
-//     required: true,
-//   },
-//   cover: {
-//     type: String, // img url?
-//     required: true,
-//   },
-//   rating: {
-//     type: Number,
-//     required: true,
-//   },
-//   review: {
-//     type: String,
-//     required: true,
-//   },
-// });
-// const model = mongoose.model("Data", landingFeedSchema);
-
-// router.get("/retrieve", async (req, res) => {
-//   try {
-//     const data = await model.find({}).exec();
-//     if (data.length === 0) {
-//       const response = await axios.get(
-//         "https://api.mockaroo.com/api/d8caa150?count=3&key=9b1fc5d0"
-//       );
-//       const responseData = response.data;
-//       await model.insertMany(responseData);
-//       res.json(resdata);
-//     } else {
-//       res.json(data);
-//     }
-//   } catch (error) {
-//     console.error("Error: ", error);
-//     res.status(500).json({ error: "Internal Server Error" });
-//   }
-// });
-
-// module.exports = router;
-
-//ADD CODE SO THAT IF THERE ARE NO REVIEWS IT SHOWS 3 RANDOM SONGS?
-
-//const model = mongoose.model("Data", landingFeedSchema);
-
-// router.get("/retrieve", async (req, res) => {
-//   try {
-//     const data = await model.find({}).exec();
-//     res.json(data);
-//   } catch (error) {
-//     console.error("Error: ", error);
-//     res.status(500).json({ error: "Internal Server Error" });
-//   }
-// });
-
 router.get("/topSongs", async (req, res) => {
   try {
     const topSongs = await Song.find({}).sort({ numReviews: -1 }).limit(5);
@@ -94,4 +30,6 @@ router.get("/topSongs", async (req, res) => {
   }
 });
 
-module.exports = router;
+module.exports = router; 
+
+
